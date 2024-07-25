444d1996b8ede5e5a675fad38372192b25d577b8
liyb <1055025755@qq.com>
2024-07-25 19:26:07 +0800

    test: husky

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,7 @@ import { ref } from 'vue';
 import { getUserInfo } from '@/api/user';
 import { useTokenStore } from '@/store/useTokenStore';
 const token = useTokenStore().token;
+
 const count = ref(0);
 
 // 服务端加载
